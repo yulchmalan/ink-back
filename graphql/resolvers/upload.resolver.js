03a83f41786f5b9b212ee657349510bda8a84416
yulch <159718838+yulchmalan@users.noreply.github.com>
2025-05-18 19:10:13 +0300

    Delete graphql directory

--- graphql/resolvers/upload.resolver.js
+++
@@ -1,9 +0,0 @@
-import { generateUploadUrl } from "../../lib/s3.js";
-
-export default {
-  Mutation: {
-    generateUploadUrl: async (_, { folder, fileName, fileType }) => {
-      return await generateUploadUrl(folder, fileName, fileType);
-    },
-  },
-};
